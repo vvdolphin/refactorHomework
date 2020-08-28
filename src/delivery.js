efab13a5ab54215bee48d902dd0e750373db7930
WANGZO <1273027520@qq.com>
2020-08-28 19:03:37 +0800

    [Zoe]extract method getRushDeliveryTime and getDeliveryTime

--- src/delivery.js
+++ src/delivery.js
@@ -1,41 +1,10 @@
 function deliveryDate (anOrder, isRush) {
   if (isRush) {
-    let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 1;
-    }
-    else if ([
-      'NY',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else {
-      deliveryTime = 3;
-    }
+    let deliveryTime = getRushDeliveryTime(anOrder);
     return anOrder.placedOn.plusDays(1 + deliveryTime);
   }
   else {
-    let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-      'NY',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else if ([
-      'ME',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    }
-    else {
-      deliveryTime = 4;
-    }
+    let deliveryTime = getDeliveryTime(anOrder);
     return anOrder.placedOn.plusDays(2 + deliveryTime);
   }
 }
@@ -44,4 +13,45 @@ module.exports= {
   deliveryDate,
 }
 
+
+function getDeliveryTime(anOrder) {
+  let deliveryTime;
+  if ([
+    'MA',
+    'CT',
+    'NY',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  }
+  else if ([
+    'ME',
+    'NH',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 3;
+  }
+  else {
+    deliveryTime = 4;
+  }
+  return deliveryTime;
+}
+
+function getRushDeliveryTime(anOrder) {
+  let deliveryTime;
+  if ([
+    'MA',
+    'CT',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 1;
+  }
+  else if ([
+    'NY',
+    'NH',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  }
+  else {
+    deliveryTime = 3;
+  }
+  return deliveryTime;
+}
 
--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -1,6 +1,5 @@
 const test = require('ava');
-const {deliveryDate} = require('../src/delivery');
-
+const {deliveryDate} = require('../src/delivery')
 
 const anOrder ={
     'deliveryState':'',
@@ -15,6 +14,71 @@ test('test1 MA true', t=>{
     let number = deliveryDate(anOrder,true);
     t.is(2,number);
 });
+
+test('test2 CT true', t=>{
+    anOrder.deliveryState ='CT';
+    let number = deliveryDate(anOrder,true);
+    t.is(2,number);
+});
+
+test('test3 NY true', t=>{
+    anOrder.deliveryState ='NY';
+    let number = deliveryDate(anOrder,true);
+    t.is(3,number);
+});
+
+test('test4 NH true', t=>{
+    anOrder.deliveryState ='NH';
+    let number = deliveryDate(anOrder,true);
+    t.is(3,number);
+});
+
+
+test('test5 TT true', t=>{
+    anOrder.deliveryState ='TT';
+    let number = deliveryDate(anOrder,true);
+    t.is(4,number);
+});
+
+test('test6 CT false', t=>{
+    anOrder.deliveryState ='CT';
+    let number = deliveryDate(anOrder,false);
+    t.is(4,number);
+});
+
+test('test7 MA false', t=>{
+    anOrder.deliveryState ='MA';
+    let number = deliveryDate(anOrder,false);
+    t.is(4,number);
+});
+
+test('test8 NY false', t=>{
+    anOrder.deliveryState ='NY';
+    let number = deliveryDate(anOrder,false);
+    t.is(4,number);
+});
+
+test('test9 ME false', t=>{
+    anOrder.deliveryState ='ME';
+    let number = deliveryDate(anOrder,false);
+    t.is(5,number);
+});
+
+test('test10 NH false', t=>{
+    anOrder.deliveryState ='NH';
+    let number = deliveryDate(anOrder,false);
+    t.is(5,number);
+});
+
+
+test('test11 TT false', t=>{
+    anOrder.deliveryState ='TT';
+    let number = deliveryDate(anOrder,false);
+    t.is(6,number);
+});
+
+
+
 
 
 
